daa76b907866bce6e3217e370007244264ce0ef2
Guilherme Monteiro Peixoto <gomp@ic.ufal.br>
2022-02-21 22:57:45 -0300

    Update client.py

--- client.py
+++ client.py
@@ -20,7 +20,7 @@ class Button:
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-        font = pygame.font.SysFont("arial", 40)
+        font = pygame.font.SysFont("comicsans", 40)
         text = font.render(self.text, 1, (255,255,255))
         win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
 
@@ -33,16 +33,15 @@ class Button:
             return False
 
 
-
 def redrawWindow(win, game, p):
-    win.fill((83,134,139))
+    win.fill((128,128,128))
 
     if not(game.connected()):
-        font = pygame.font.SysFont("arial", 80)
+        font = pygame.font.SysFont("comicsans", 80)
         text = font.render("Waiting for Player...", 1, (255,0,0), True)
         win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
     else:
-        font = pygame.font.SysFont("arial", 60)
+        font = pygame.font.SysFont("comicsans", 60)
         text = font.render("Your Move", 1, (0, 255,255))
         win.blit(text, (80, 200))
 
@@ -82,7 +81,7 @@ def redrawWindow(win, game, p):
     pygame.display.update()
 
 
-btns = [Button("Pedra", 50, 500, (0,0,0)), Button("Tesoura", 250, 500, (255,0,0)), Button("Papel", 450, 500, (0,255,0))]
+btns = [Button("Rock", 50, 500, (0,0,0)), Button("Scissors", 250, 500, (255,0,0)), Button("Paper", 450, 500, (0,255,0))]
 def main():
     run = True
     clock = pygame.time.Clock()
@@ -109,13 +108,13 @@ def main():
                 print("Couldn't get game")
                 break
 
-            font = pygame.font.SysFont("arial", 90)
+            font = pygame.font.SysFont("comicsans", 90)
             if (game.winner() == 1 and player == 1) or (game.winner() == 0 and player == 0):
-                text = font.render("Venceu!", 1, (255,0,0))
+                text = font.render("You Won!", 1, (255,0,0))
             elif game.winner() == -1:
-                text = font.render("Empate", 1, (255,0,0))
+                text = font.render("Tie Game!", 1, (255,0,0))
             else:
-                text = font.render("Perdeu", 1, (255, 0, 0))
+                text = font.render("You Lost...", 1, (255, 0, 0))
 
             win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
             pygame.display.update()
@@ -146,8 +145,8 @@ def menu_screen():
     while run:
         clock.tick(60)
         win.fill((128, 128, 128))
-        font = pygame.font.SysFont("arial", 60)
-        text = font.render("Aperte para jogar", 1, (255,0,0))
+        font = pygame.font.SysFont("comicsans", 60)
+        text = pygame.image.load("/home/guilherme/Área de Trabalho/4144475.png").convert_alpha()
         win.blit(text, (100,200))
         pygame.display.update()
 
